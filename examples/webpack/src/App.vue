41ede5d85aaccd354aa64fbcbe782e65f982892c
Lupu Alexandru Stefan <alex.loopu@gmail.com>
2020-11-30 18:34:43 +0200

    🚧 Add example files for webpack

---
+++ examples/webpack/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app">
+    <div class="container">
+      <ul class="set">
+        <li v-for="item in initialSet" :key="item" class="set__item">{{ item }}</li>
+      </ul>
+    </div>
+    <div class="container">
+      <ul class="set">
+        <li v-for="item in derangedSet" :key="item" class="set__item">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import derange from '../../../';
+const set = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+export default {
+  data: () => {
+    return {
+      initialSet: set,
+      derangedSet: derange(set),
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ examples/webpack/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
